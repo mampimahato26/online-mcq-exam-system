7add11e0b667d1d95fba2acc6b85891935d0c24d
mampimahato26 <mampimahato727@gmail.com>
2026-08-26 13:00:06 +0530

    Update MySQL connection for Aiven

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2/promise');
+
 require('dotenv').config();
 
-// Create connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mcq_exam_system',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
